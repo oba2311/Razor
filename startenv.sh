a4f10fd2221f99052a14f2902b964a7817e22726
oba2311 <oba2311@gmail.com>
2019-09-10 01:24:02 +0300

    tidying up docker files.

--- startenv.sh
+++ startenv.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd /Users/oba2311/Desktop/Razor/TFContainer
+
 docker build -t devenv .
 
-echo "your docker env is starting..."
+echo "your docker env is starting and jupyter notebook would open at 0.0.0.0"
